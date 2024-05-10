3b9af69abcb2b7761936695e8f6a907215c986bd
landaguo <lan_daguo@163.com>
2024-05-10 17:07:10 +0800

    2024.05.10刷题

---
+++ src/OD/数组连续和.java
@@ -0,0 +1,46 @@
+package OD;
+
+import java.util.Scanner;
+
+/**
+ * @Description：
+ * @Date：2024-05-10
+ * @Author：landaguo
+ */
+public class 数组连续和 {
+
+    /**
+     * 3 7
+     * 3 4 7
+     *
+     * 动态规划
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int[] preSum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + arr[i - 1];
+        }
+        int l = 0;
+        int r = 1;
+        long ans = 0;
+        while (r <= n) {
+            if (preSum[r] - preSum[l] >= sum) {
+                ans += n - r + 1;
+                l++;
+                r = l +1;
+            }else{
+                r++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
