34e9c63f6708018886b0202df3e9373b7b9f3085
landaguo <lan_daguo@163.com>
2024-06-03 20:06:34 +0800

    2024.06.03刷题

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,7 +1,6 @@
 package test;
 
 import java.util.*;
-
 /**
  * @Description：
  * @Date：2024-05-09
@@ -11,31 +10,22 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        int sum = scanner.nextInt();
-
-        int ans = 0;
-        int l = 0;
-        int r = 0;
-        int window_sum = 0;
-
-        while (r < n) {
-            if (window_sum + arr[r] <= sum) {
-                window_sum += arr[r++];
-            }else {
-                ans = Math.max(ans,r - l);
-                window_sum -= arr[l++];
+        int[] data = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] result = Arrays.copyOf(data,data.length);
+        int i = 0;
+        int j = 1;
+        while (i != j) {
+            if (j == data.length - 1) {
+                j = 0;
+            }
+            if (data[j] < data[i]) {
+                result[i] += result[j];
+                i++;
+            }else{
+                j++;
             }
         }
-        if (window_sum <= sum){
-            ans = Math.max(ans,r - l);
-        }
-        System.out.println(ans);
-    }
 
+    }
 }
 
