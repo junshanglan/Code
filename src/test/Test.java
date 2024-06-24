7f99dd2d7fb5800636ba05ae48eb4decf4e09ecf
landaguo <lan_daguo@163.com>
2024-06-24 09:11:24 +0800

    2024.06.23考试

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,24 +8,7 @@ import java.util.*;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] data = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] result = Arrays.copyOf(data,data.length);
-        int i = 0;
-        int j = 1;
-        while (i != j) {
-            if (j == data.length - 1) {
-                j = 0;
-            }
-            if (data[j] < data[i]) {
-                result[i] += result[j];
-                i++;
-            }else{
-                j++;
-            }
-        }
 
-    }
+
 }
 
