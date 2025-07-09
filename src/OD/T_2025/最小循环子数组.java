ee474705ec35055699ab1a99447bf6540b9973b5
landaguo <lan_daguo@163.com>
2025-07-09 20:57:45 +0800

    2025.07.09-取余方式对应数组的位序，比较数字大小，比较字典序

---
+++ src/OD/T_2025/最小循环子数组.java
@@ -0,0 +1,40 @@
+package OD.T_2025;
+
+import java.util.Scanner;
+
+/**
+ * @Description：取余的方式取到数组开始的几位
+ * @Date：2025-07-09
+ * @Author：landaguo
+ */
+public class 最小循环子数组 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int count = 1; count < n; count++) {
+            if (n % count != 0) continue;
+            boolean flag = true;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] != arr[i % count]){
+                     flag = false;
+                     break;
+                }
+            }
+            if(flag) {
+                for (int i = 0; i < count; i++) {
+                    System.out.print(arr[i]);
+                    System.out.print(" ");
+                    return;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+    }
+}
