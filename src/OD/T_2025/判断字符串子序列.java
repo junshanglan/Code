48932b4ca692de6bea082bddadc70bda82209a0f
landaguo <lan_daguo@163.com>
2025-07-17 20:27:14 +0800

    07.14-字符串双指针问题

---
+++ src/OD/T_2025/判断字符串子序列.java
@@ -0,0 +1,33 @@
+package OD.T_2025;
+
+import java.util.Scanner;
+
+/**
+ * 使用双指针法
+ */
+public class 判断字符串子序列 {
+    /**
+     * abc
+     * abcaybec
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String target = scanner.nextLine();
+        String source = scanner.nextLine();
+
+        int t = target.length() - 1;
+        int s = source.length() - 1;
+        while (t >= 0 && s >= 0) {
+            if(target.charAt(t) == source.charAt(s)) {
+                t--;
+            }
+            s--;
+        }
+
+        if (s < 0) {
+            System.out.println(-1);
+        }else{
+            System.out.println(s + 1);
+        }
+    }
+}
---
+++ src/OD/T_2025/字符串序列判定2.java
@@ -0,0 +1,29 @@
+package OD.T_2025;
+
+import java.util.Scanner;
+
+/**
+ * 直接遍历字符串判定，区别于双指针法
+ */
+public class 字符串序列判定2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String l = scanner.nextLine();
+        if (s.length() == 0) {
+            System.out.println(-1);
+            return ;
+        }
+        int result = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (l.indexOf(c) != -1) {
+                result = l.indexOf(c);
+                continue;
+            }
+            break;
+        }
+        System.out.println(result);
+    }
+}
