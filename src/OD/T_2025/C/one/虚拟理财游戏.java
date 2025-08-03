91714655180beac6e0f6382f80856a6cc1473be8
landaguo <lan_daguo@163.com>
2025-08-03 23:43:08 +0800

    2025.08.03 - 简单题

--- src/OD/T_2025/C/one/虚拟理财游戏.java
+++ src/OD/T_2025/C/one/虚拟理财游戏.java
@@ -11,7 +11,7 @@ public class 虚拟理财游戏 {
         // 产品数
         int n = scanner.nextInt();
         // 总投资额
-        int total = scanner.nextInt();
+        int investAmount = scanner.nextInt();
         // 可以接受的总风险
         int riskTotal = scanner.nextInt();
         // 产品投资的回报
@@ -19,7 +19,7 @@ public class 虚拟理财游戏 {
         // 产品的风险
         int[] riskArr = new int[n];
         // 最大投资额序列
-        int[] edu = new int[n];
+        int[] investArr = new int[n];
         for (int i = 0; i < n; i++) {
             huibao[i] = scanner.nextInt();
         }
@@ -27,7 +27,43 @@ public class 虚拟理财游戏 {
             riskArr[i] = scanner.nextInt();
         }
         for (int i = 0; i < n; i++) {
-            edu[i] = scanner.nextInt();
+            investArr[i] = scanner.nextInt();
+        }
+        // 记录最优策略的分配数组
+        int[] res = new int[n];
+        int max = 0;
+        // 投资单个产品,投资额度要够，且能够承受风险
+        for (int i = 0; i < n; i++) {
+            if (riskTotal >= riskArr[i] && investAmount >= investArr[i]){
+                int[] tmp = new int[n];
+                if (huibao[i] > max){
+                    max = huibao[i];
+                    tmp[i] = investArr[i];
+                    res = tmp;
+                }
+            }
+        }
+        // 投资两个产品组合,投资额度要够，且能够承受风险
+        for (int i = 0; i < n; i++) {
+            int[] tmp = new int[n];
+            int riskRemain = riskTotal - riskArr[i];
+            int investRemain = investAmount -  investArr[i];
+            if (riskRemain > 0 && investRemain > 0){
+                for (int j = i + 1; j < n; j++) {
+                    if (riskRemain - riskArr[j] >= 0 && investRemain - investArr[j] >= 0) {
+                        int sum = huibao[i] + huibao[j];
+                        if (sum > max){
+                            max = sum;
+                            tmp[i] = investArr[i];
+                            tmp[j] = investArr[j];
+                            res = tmp;
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
         }
     }
 }
