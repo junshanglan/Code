acec5924ad636b40142d1c8fb0fce7f80bf1b21a
landaguo <lan_daguo@163.com>
2025-09-25 09:20:48 +0800

    0925

---
+++ src/OD/T_2025/C/two/模拟/推荐多样性.java
@@ -0,0 +1,11 @@
+package OD.T_2025.C.two.模拟;
+
+import java.util.Scanner;
+
+public class 推荐多样性 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
